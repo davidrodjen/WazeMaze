992c64701c1052c0523e3cbd48109c4993fdf9d2
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-12-17 14:05:03 -0800

    Add project files.

---
+++ WazeMaze/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace WazeMaze
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.b2 = new System.Windows.Forms.Label();
+            this.a2 = new System.Windows.Forms.Label();
+            this.b1 = new System.Windows.Forms.Label();
+            this.a1 = new System.Windows.Forms.Label();
+            this.leftBorder = new System.Windows.Forms.Label();
+            this.rightBorder = new System.Windows.Forms.Label();
+            this.topBorder = new System.Windows.Forms.Label();
+            this.arrow = new System.Windows.Forms.Label();
+            this.bottomBorder = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.b2.Location = new System.Drawing.Point(289, 96);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(13, 218);
+            this.b2.TabIndex = 17;
+            this.b2.Text = "label1";
+            // 
+            // a2
+            // 
+            this.a2.BackColor = System.Drawing.SystemColors.Desktop;
+            this.a2.Location = new System.Drawing.Point(88, 96);
+            this.a2.Name = "a2";
+            this.a2.Size = new System.Drawing.Size(174, 10);
+            this.a2.TabIndex = 16;
+            this.a2.Text = "label1";
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.b1.Location = new System.Drawing.Point(513, 21);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(13, 218);
+            this.b1.TabIndex = 15;
+            this.b1.Text = "label1";
+            // 
+            // a1
+            // 
+            this.a1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.a1.Location = new System.Drawing.Point(26, 160);
+            this.a1.Name = "a1";
+            this.a1.Size = new System.Drawing.Size(174, 10);
+            this.a1.TabIndex = 14;
+            this.a1.Text = "label1";
+            // 
+            // leftBorder
+            // 
+            this.leftBorder.BackColor = System.Drawing.SystemColors.Desktop;
+            this.leftBorder.Location = new System.Drawing.Point(14, 9);
+            this.leftBorder.Name = "leftBorder";
+            this.leftBorder.Size = new System.Drawing.Size(14, 592);
+            this.leftBorder.TabIndex = 13;
+            this.leftBorder.Text = "label4";
+            // 
+            // rightBorder
+            // 
+            this.rightBorder.BackColor = System.Drawing.SystemColors.Desktop;
+            this.rightBorder.Location = new System.Drawing.Point(757, 21);
+            this.rightBorder.Name = "rightBorder";
+            this.rightBorder.Size = new System.Drawing.Size(10, 582);
+            this.rightBorder.TabIndex = 11;
+            this.rightBorder.Text = "label2";
+            // 
+            // topBorder
+            // 
+            this.topBorder.BackColor = System.Drawing.SystemColors.Desktop;
+            this.topBorder.Location = new System.Drawing.Point(26, 9);
+            this.topBorder.Name = "topBorder";
+            this.topBorder.Size = new System.Drawing.Size(741, 10);
+            this.topBorder.TabIndex = 10;
+            this.topBorder.Text = "label1";
+            // 
+            // arrow
+            // 
+            this.arrow.AutoSize = true;
+            this.arrow.Font = new System.Drawing.Font("Webdings", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.arrow.Location = new System.Drawing.Point(48, 43);
+            this.arrow.Name = "arrow";
+            this.arrow.Size = new System.Drawing.Size(28, 22);
+            this.arrow.TabIndex = 9;
+            this.arrow.Text = "O";
+            // 
+            // bottomBorder
+            // 
+            this.bottomBorder.BackColor = System.Drawing.SystemColors.Desktop;
+            this.bottomBorder.Location = new System.Drawing.Point(26, 590);
+            this.bottomBorder.Name = "bottomBorder";
+            this.bottomBorder.Size = new System.Drawing.Size(691, 11);
+            this.bottomBorder.TabIndex = 18;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 621);
+            this.Controls.Add(this.bottomBorder);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.a2);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.a1);
+            this.Controls.Add(this.leftBorder);
+            this.Controls.Add(this.rightBorder);
+            this.Controls.Add(this.topBorder);
+            this.Controls.Add(this.arrow);
+            this.Name = "Form1";
+            this.Text = "WazeMaze";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label b2;
+        private System.Windows.Forms.Label a2;
+        private System.Windows.Forms.Label b1;
+        private System.Windows.Forms.Label a1;
+        private System.Windows.Forms.Label leftBorder;
+        private System.Windows.Forms.Label rightBorder;
+        private System.Windows.Forms.Label topBorder;
+        private System.Windows.Forms.Label arrow;
+        private System.Windows.Forms.Label bottomBorder;
+    }
+}
+
---
+++ WazeMaze/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WazeMaze
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            arrow.Text = "J";
+            arrow.Location = new Point(30, 40);
+
+            this.KeyDown += new KeyEventHandler(MainForm_KeyInput);
+        }
+
+        public void MainForm_KeyInput(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Left)
+            {
+                arrow.Location = new Point(arrow.Location.X - 4, arrow.Location.Y);
+            }
+
+            if (e.KeyData == Keys.Right)
+            {
+                arrow.Location = new Point(arrow.Location.X + 4, arrow.Location.Y);
+            }
+
+            if (e.KeyData == Keys.Up)
+            {
+                arrow.Location = new Point(arrow.Location.X, arrow.Location.Y - 4);
+            }
+
+            if (e.KeyData == Keys.Down)
+            {
+                arrow.Location = new Point(arrow.Location.X, arrow.Location.Y + 4);
+            }
+
+            //If the arrow touches the boarders
+            if (topBorder.Bounds.IntersectsWith(arrow.Bounds) || bottomBorder.Bounds.IntersectsWith(arrow.Bounds)
+             || leftBorder.Bounds.IntersectsWith(arrow.Bounds) || rightBorder.Bounds.IntersectsWith(arrow.Bounds))
+            {
+                GameOver();
+            }
+
+            if (a1.Bounds.IntersectsWith(arrow.Bounds) || a2.Bounds.IntersectsWith(arrow.Bounds)
+             || b1.Bounds.IntersectsWith(arrow.Bounds) || b2.Bounds.IntersectsWith(arrow.Bounds))
+            {
+                GameOver();
+            }
+        }
+
+
+        private void GameOver()
+        {
+            MessageBox.Show("You failed, back to the start");
+
+            arrow.Location = new Point(48, 43);
+        }
+    }
+}
